0200b392a9b2eb43870216e967562068645f22b6
矢吹祐真 <yumaadobe@gmail.com>
2016-08-31 01:45:03 +0900

    Initial Commit

---
+++ niftyTestTests/niftyTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  niftyTestTests.swift
+//  niftyTestTests
+//
+//  Created by 矢吹祐真 on 2016/08/31.
+//  Copyright © 2016年 矢吹祐真. All rights reserved.
+//
+
+import XCTest
+@testable import niftyTest
+
+class niftyTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
